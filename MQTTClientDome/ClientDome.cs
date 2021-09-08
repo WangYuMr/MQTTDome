3aceb5d6935ff4b88ea5705b3a6cf207f8757119
羽 王 <1344695152@qq.com>
2021-09-08 20:24:34 +0800

    代码提交

--- MQTTClientDome/ClientDome.cs
+++ MQTTClientDome/ClientDome.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using MQTTDome.Interface;
+﻿using MQTTDome.Interface;
 using MQTTDomeMode;
 using MQTTnet;
 using MQTTnet.Adapter;
@@ -10,7 +7,12 @@ using MQTTnet.Client.Connecting;
 using MQTTnet.Client.Disconnecting;
 using MQTTnet.Client.Options;
 using MQTTnet.Client.Receiving;
-using MQTTnet.Exceptions;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MQTTClientDome
 {
@@ -18,6 +20,7 @@ namespace MQTTClientDome
     {
         private IMqttClient client;
         private readonly MqttClientEntity model;
+        private IMqttClientOptions options;
         public ClientDome(MqttClientEntity mqttClientEntity) {
             model = mqttClientEntity;
         }
@@ -28,16 +31,26 @@ namespace MQTTClientDome
             {
                 client = new MqttFactory().CreateMqttClient();
                 var build = new MqttClientOptionsBuilder()
+                 //配置客户端Id
                 .WithClientId(model.ClientId)
+                //配置登录账号
                 .WithCredentials(model.Account, model.PassWord)
-                .WithTcpServer(model.IP, model.Port);
-                IMqttClientOptions options = build.Build();
+                //配置服务器IP端口 这里得端口号是可空的
+                .WithTcpServer(model.IP, model.Port)
+                // .WithTcpServer("www.baidu.com")这是一个例子
+                ;
+
+                 options = build.Build();
+                //收到服务器发来消息
                 client.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(MessageReceivedHandler);
+                //连接成功 
                 client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(ConnectedHandler);
+                //断开连接 重连就写在此处
                 client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(DisconnectedHandler);
+                //连接
                 await client.ConnectAsync(options);
             }
-            catch (MqttConnectingFailedException ex) {
+            catch (MqttConnectingFailedException) {
                 Console.WriteLine("身份校验失败");
             }
             catch (Exception ex)
@@ -47,15 +60,28 @@ namespace MQTTClientDome
                 Console.WriteLine(ex.Message);
             }
         }
-
+        /// <summary>
+        /// 客户端断开连接后,如果需要重连在此处实现
+        /// </summary>
+        /// <param name="obj"></param>
         private void DisconnectedHandler(MqttClientDisconnectedEventArgs obj)
         {
             Console.WriteLine("本客户端已经断开连接");
             Console.WriteLine();
+            try
+            {
+                client.ConnectAsync(options).GetAwaiter().GetResult();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("重连失败");
+            }
+         
+
         }
 
         /// <summary>
-        /// 连接成功
+        /// 连接成功 在此处做订阅主题(Topic)操作
         /// </summary>
         /// <param name="obj"></param>
         private void ConnectedHandler(MqttClientConnectedEventArgs obj)
@@ -66,6 +92,20 @@ namespace MQTTClientDome
             Console.WriteLine($"客户端:{model.ClientId}");
             Console.WriteLine($"账号:{model.Account}");
             Console.WriteLine();
+            //第1种订阅方式
+            client.SubscribeAsync("主题名称").GetAwaiter().GetResult();
+
+            //第2种订阅方式
+            List<MqttTopicFilter> Topics = new List<MqttTopicFilter>();
+            Topics.Add(new MqttTopicFilter() { Topic= "主题名称A", QualityOfServiceLevel= MqttQualityOfServiceLevel.ExactlyOnce});
+            Topics.Add(new MqttTopicFilter() { Topic = "主题名称B" });
+            Topics.Add(new MqttTopicFilter() { Topic = "主题名称C" });
+            client.SubscribeAsync(Topics.ToArray()).GetAwaiter().GetResult();
+
+            //第3种订阅方式
+            MqttClientSubscribeOptionsBuilder builder = new MqttClientSubscribeOptionsBuilder();
+            builder.WithTopicFilter("AAA");
+            client.SubscribeAsync(builder.Build()).GetAwaiter().GetResult();
         }
 
         /// <summary>
@@ -80,6 +120,8 @@ namespace MQTTClientDome
             Console.WriteLine($"消息:{Encoding.UTF8.GetString(obj.ApplicationMessage.Payload)}");
             Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++");
             Console.WriteLine();
+
+
         }
 
         public async Task StopAsync()
